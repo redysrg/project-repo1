c179d39dcca13b1d96ba84ec205c2a8156f7f328
redysrg <redysrg@gmail.com>
2024-07-28 14:40:22 -0700

    Update App.java 2nd time 390

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! updated1" );
+        System.out.println( "Hello World! updated 2nd time to run with 3.9.0" );
     }
 }
