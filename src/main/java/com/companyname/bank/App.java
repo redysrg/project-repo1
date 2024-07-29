02c70c5a6010b5e28a5441966ccb6e2cf803d775
redysrg <redysrg@gmail.com>
2024-07-28 20:28:13 -0700

    git-scm pollin- wenhooks

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! updated 3rd time to run with 3.8.1..for poll SCm1" );
+        System.out.println( "Hello World! updated 4th time for 3rd time -for git-scm pollin- wenhooks" );
     }
 }
